f9fd62ed9993da257324c4650625784ab49b3cc6
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-01-13 10:20:46 +0200

    ReverseNumbersWithAStack solution

--- stacks-and-queues/src/ReverseNumbersWithAStack/Main.java
+++ stacks-and-queues/src/ReverseNumbersWithAStack/Main.java
@@ -1,2 +1,24 @@
-package ReverseNumbersWithAStack;public class Main {
+package ReverseNumbersWithAStack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int number : numbers) {
+            stack.push(number);
+        }
+
+        while(stack.size() > 0) {
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
 }
