e97d65cb8d99ab8b431bc75c3363f0e4e8fafd60
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-01-13 10:56:15 +0200

    BalancedParentheses solution

---
+++ stacks-and-queues/src/BalancedParentheses/Main.java
@@ -0,0 +1,42 @@
+package BalancedParentheses;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+
+        ArrayDeque<Character> openingParentheses = new ArrayDeque<>();
+        ArrayDeque<Character> closingParentheses = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentBracket = input.charAt(i);
+            if(currentBracket == '(' || currentBracket == '{' || currentBracket == '[') {
+                openingParentheses.offer(currentBracket);
+            } else if(currentBracket == ')' || currentBracket == '}' || currentBracket == ']') {
+                closingParentheses.push(currentBracket);
+            }
+        }
+
+        boolean areBalanced = true;
+        while(openingParentheses.size() > 0) {
+            char currentOpeningBracket = openingParentheses.poll();
+            char currentClosingBracket = closingParentheses.pop();
+            if(currentOpeningBracket == '(' && (currentClosingBracket == '}' || currentClosingBracket == ']')) {
+                areBalanced = false;
+            } else if(currentOpeningBracket == '{' && (currentClosingBracket == ')' || currentClosingBracket == ']')) {
+                areBalanced = false;
+            } else if(currentOpeningBracket == '[' && (currentClosingBracket == ')' || currentClosingBracket == '}')) {
+                areBalanced = false;
+            }
+        }
+
+        if(areBalanced) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
