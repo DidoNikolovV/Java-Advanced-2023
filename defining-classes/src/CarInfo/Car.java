3a27417ed384a7ceaf76dfdb64f22af8825f5e5e
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-01-31 09:53:11 +0200

    CarInfo added constructors

--- defining-classes/src/CarInfo/Car.java
+++ defining-classes/src/CarInfo/Car.java
@@ -5,6 +5,21 @@ public class Car {
     private String model;
     private int horsePower;
 
+    public Car() {
+
+    }
+    public Car(String brand) {
+        this.brand = brand;
+        this.model = "unknown";
+        this.horsePower = - 1;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this(brand);
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
     public String getBrand() {
         return this.brand;
     };
