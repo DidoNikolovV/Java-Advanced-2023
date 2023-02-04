8b7d9d2dcabc5b53338ea3c8b6bdb55c695c7cfa
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-02-04 13:54:17 +0200

    GenericArrayCreator exercise solution

---
+++ generics/src/GenericArrayCreator/Main.java
@@ -0,0 +1,18 @@
+package GenericArrayCreator;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strings = ArrayCreator.create(String.class,10, "none");
+        Integer[] integers = ArrayCreator.create(10, 10);
+
+        for (String string : strings) {
+            System.out.print(string + " ");
+        }
+
+        System.out.println();
+
+        for (Integer integer : integers) {
+            System.out.print(integer + " ");
+        }
+    }
+}
