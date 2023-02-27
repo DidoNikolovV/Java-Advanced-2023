8eb1e5feb354439c4bcd4f8e79df717890e9bf37
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-02-27 13:32:48 +0200

    Sort By Name and Age exercise solution

---
+++ encapsulation/src/_01_Sort_By_Name_And_Age/Person.java
@@ -0,0 +1,31 @@
+package _01_Sort_By_Name_And_Age;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName + " is " + this.age
+        + " years old.";
+    }
+}
