ab0903782f48ff7f6f7717a9ee69129a2fdd5a35
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-03-03 09:44:29 +0200

    01.Single Inheritance exercise solution

---
+++ inheritance/src/_01_Single_Inheritance/Main.java
@@ -0,0 +1,10 @@
+package _01_Single_Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
