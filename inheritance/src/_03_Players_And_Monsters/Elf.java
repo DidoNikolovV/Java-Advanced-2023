a775646e741b18e87bc17229dce63a0de807fe19
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-03-03 10:19:39 +0200

    03.Players and Monsters exercise solution

---
+++ inheritance/src/_03_Players_And_Monsters/Elf.java
@@ -0,0 +1,8 @@
+package _03_Players_And_Monsters;
+
+public class Elf extends Hero {
+
+    public Elf(String username, int level) {
+        super(username, level);
+    }
+}
---
+++ inheritance/src/_03_Players_And_Monsters/SoulMaster.java
@@ -0,0 +1,7 @@
+package _03_Players_And_Monsters;
+
+public class SoulMaster extends DarkWizard{
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
