3cd710ddb9d31eedecfcc86a84f556b58cf5f2c1
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-03-06 18:39:46 +0200

    01.CarShop exercise solution

---
+++ Interfaces-and-Abstraction/src/_01_Car_Shop/Car.java
@@ -0,0 +1,10 @@
+package _01_Car_Shop;
+
+public interface Car {
+    Integer tires = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+}
