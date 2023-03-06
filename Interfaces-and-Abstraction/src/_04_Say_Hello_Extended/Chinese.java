6d70425c3896b60617ece894645035c7e2849a4c
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-03-06 19:26:47 +0200

    04.Say Hello Extended

---
+++ Interfaces-and-Abstraction/src/_04_Say_Hello_Extended/Chinese.java
@@ -0,0 +1,12 @@
+package _04_Say_Hello_Extended;
+
+public class Chinese extends BasePerson{
+    protected Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
