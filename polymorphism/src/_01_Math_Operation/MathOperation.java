0a1165a0dfa07a579bbaab402c64654f61222d29
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-03-10 11:55:21 +0200

    MathOperation exercise solution

---
+++ polymorphism/src/_01_Math_Operation/MathOperation.java
@@ -0,0 +1,15 @@
+package _01_Math_Operation;
+
+public class MathOperation {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int add(int num1, int num2, int num3) {
+        return add(num1, num2) + num3;
+    }
+
+    public int add(int num1, int num2, int num3, int num4) {
+        return add(num1, num2, num3) + num4;
+    }
+}
