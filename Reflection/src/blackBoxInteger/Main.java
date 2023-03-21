9aaa412a1839fd01ee74a3ba7e5a2930fb7fd32c
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-03-21 19:03:03 +0200

    barracksWars-A New Factory and blackBoxInteger exercises solutions

--- Reflection/src/blackBoxInteger/Main.java
+++ Reflection/src/blackBoxInteger/Main.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -48,5 +50,6 @@ public class Main {
 
             command = scanner.nextLine();
         }
+        
     }
 }
